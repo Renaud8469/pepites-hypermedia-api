63c5dbcde5597e50afea46169b8de47e9f0354e8
Renaud Dahl <renauddahl@gmail.com>
2017-07-19 15:12:38 +0200

    Fix lint

--- api/format_converter/index.js
+++ api/format_converter/index.js
@@ -1,19 +1,19 @@
 const interceptor = require('express-interceptor')
 
 const halInterceptor = interceptor(function(req, res) {
-	return {
-		isInterceptable : function() {
-			return /application\/vnd\.hal\+json/.test(req.get('Accept'));
-		},
-		intercept: function (body, send) {
-			res.set('Content-type', 'application/vnd.hal+json');
-			obj = JSON.parse(body);
-			obj.media_type = "I'm afraid I can't let you do that.";
-			send(JSON.stringify(obj));
-		}
-	}
-});
+  return {
+    isInterceptable : function() {
+      return /application\/vnd\.hal\+json/.test(req.get('Accept'))
+    },
+    intercept: function (body, send) {
+      res.set('Content-type', 'application/vnd.hal+json')
+      let obj = JSON.parse(body)
+      obj.media_type = 'I\'m afraid I can\'t let you do that.'
+      send(JSON.stringify(obj))
+    }
+  }
+})
 
-module.exports = halInterceptor;
+module.exports = halInterceptor
 
 
